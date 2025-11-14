d50187f28c626ce867a69a818444a36ba38280ad
Alperen Oncul <ncl.alperen@gmail.com>
2025-11-15 02:43:33 +0300

    modified:   src/analysis.c 	modified:   src/fdtd_core.c 	modified:   tests/unit/test_analysis_alloc.c 	modified:   tests/unit/test_fdtd_core.c

--- tests/unit/test_fdtd_core.c
+++ tests/unit/test_fdtd_core.c
@@ -3,6 +3,7 @@
 
 #include <check.h>
 #include <math.h>
+#include <stdlib.h>
 
 START_TEST(test_dt_uses_requested_safety) {
     double dx = 0.01;
@@ -39,6 +40,32 @@ START_TEST(test_fdtd_init_handles_partial_alloc_failure) {
 }
 END_TEST
 
+START_TEST(test_fdtd_free_handles_partially_initialized_state) {
+    SimulationState* state = (SimulationState*)calloc(1, sizeof(SimulationState));
+    ck_assert_ptr_nonnull(state);
+    state->nx = 1;
+    state->ny = 1;
+    state->Ez = (double**)calloc(1, sizeof(double*));
+    state->Ez_data = (double*)calloc(1, sizeof(double));
+    ck_assert_ptr_nonnull(state->Ez);
+    ck_assert_ptr_nonnull(state->Ez_data);
+    state->Ez[0] = state->Ez_data;
+    fdtd_free(state);
+}
+END_TEST
+
+START_TEST(test_fdtd_init_handles_allocation_sequence_failures) {
+    for (int fail_after = 0; fail_after < 20; ++fail_after) {
+        fdtd_test_set_alloc_fail_after(fail_after);
+        ck_assert_ptr_null(fdtd_init(NULL));
+    }
+    fdtd_test_set_alloc_fail_after(-1);
+    SimulationState* sim = fdtd_init(NULL);
+    ck_assert_ptr_nonnull(sim);
+    fdtd_free(sim);
+}
+END_TEST
+
 static Suite* fdtd_suite(void) {
     Suite* s = suite_create("fdtd_core");
     TCase* tc = tcase_create("core");
@@ -46,6 +73,8 @@ static Suite* fdtd_suite(void) {
     tcase_add_test(tc, test_dt_falls_back_to_default);
     tcase_add_test(tc, test_fdtd_init_handles_state_alloc_failure);
     tcase_add_test(tc, test_fdtd_init_handles_partial_alloc_failure);
+    tcase_add_test(tc, test_fdtd_free_handles_partially_initialized_state);
+    tcase_add_test(tc, test_fdtd_init_handles_allocation_sequence_failures);
     suite_add_tcase(s, tc);
     return s;
 }
